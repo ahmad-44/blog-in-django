328a16688a334015757ac5e526d12c6d2fc16e77
Ahmad Ali <ahmadalibaffa@gmail.com>
2024-01-02 23:31:32 +0500

    Createad feature of Saving posts for later read by using sessions

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts", views.AllPostView.as_view(), name="post-page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(),
-         name="post-detail-page")  # /posts/my-first-post
+         name="post-detail-page"),  # /posts/my-first-post
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
